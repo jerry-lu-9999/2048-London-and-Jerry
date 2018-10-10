a625aa789dad4f30dfa2f7c9af3a8ee37176fccc
Bmoist <lye11@u.rochester.edu>
2018-10-09 21:21:23 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,10 +1,20 @@
+import javax.swing.*;
 
 public class Game {
 	
+	static Block2048 canvas;
+	
 	public static void main(String[]args) {
+		JFrame frame = new JFrame();
+		canvas = new Block2048();
 		
-		Block2048 game1 = new Block2048();
-		game1.printGameBoard();
+		frame.add(canvas);
+		frame.setSize(150,1);
+		frame.setLocation(400,100);
+		frame.setTitle("2048");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(canvas);
+		frame.setVisible(true);
 		
 	}
 }
